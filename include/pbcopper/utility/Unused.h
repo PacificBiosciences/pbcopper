f2430f03088f6ee09f8976488f82fd50a0fcf553
Derek Barnett <dbarnett@pacificbiosciences.com>
2017-09-26 09:00:03 -0500

    Moved UNUSED from macro to namespace-scoped method

---
+++ include/pbcopper/utility/Unused.h
@@ -0,0 +1,12 @@
+#ifndef PBCOPPER_UTILITY_UNUSED_H
+#define PBCOPPER_UTILITY_UNUSED_H
+
+namespace PacBio {
+namespace Utility {
+
+template<typename T> void UNUSED(const T&) { }
+
+} // namespace Utility
+} // namespace PacBio
+
+#endif // PBCOPPER_UTILITY_UNUSED_H
