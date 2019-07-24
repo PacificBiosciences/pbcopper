c98492a09e6976c57d52060e89d7ab67fe534725
Armin Töpfer <atoepfer@pacificbiosciences.com>
2019-07-24 13:39:55 -0700

    Fix FaF
    If there is an exception it is rethrown in the next
    ProduceWith() or in Finalize().
    
    Wait until all tasks have been finished in Finalize().

--- tests/src/parallel/test_FireAndForget.cpp
+++ tests/src/parallel/test_FireAndForget.cpp
@@ -1,9 +1,11 @@
 // Author: Armin Töpfer
 #include <pbcopper/parallel/FireAndForget.h>
 
+#include <chrono>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 #include <vector>
 
 #include <gtest/gtest.h>
@@ -14,7 +16,11 @@ TEST(Parallel_FireAndForget, strings)
     static const size_t numElements = 10000;
     PacBio::Parallel::FireAndForget faf(numThreads);
 
-    auto Submit = [](std::string& input) { input = "done-" + input; };
+    std::atomic_int waiting{0};
+    auto Submit = [&waiting](std::string& input) {
+        input = "done-" + input;
+        --waiting;
+    };
 
     std::vector<std::string> vec;
 
@@ -22,13 +28,66 @@ TEST(Parallel_FireAndForget, strings)
         vec.emplace_back(std::to_string(i));
     }
 
-    for (auto& v : vec)
-        faf.ProduceWith(Submit, std::ref(v));
+    for (auto& v : vec) {
+        ++waiting;
+        EXPECT_NO_THROW(faf.ProduceWith(Submit, std::ref(v)));
+    }
 
-    faf.Finalize();
+    EXPECT_NO_THROW(faf.Finalize());
+    EXPECT_EQ(waiting, 0);
 
     for (auto& v : vec)
         EXPECT_EQ(v.substr(0, 4), "done");
 
     EXPECT_EQ(vec.size(), numElements);
+}
+
+TEST(Parallel_FireAndForget, exceptionFinalize)
+{
+    static const size_t numThreads = 3;
+    PacBio::Parallel::FireAndForget faf(numThreads, 1);
+
+    std::atomic_int counter{0};
+    auto SubmitSleep = [&counter]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        ++counter;
+    };
+    auto SubmitExc = [&counter]() {
+        throw std::runtime_error("faf abort");
+        ++counter;
+    };
+
+    EXPECT_NO_THROW(faf.ProduceWith(SubmitSleep));
+    EXPECT_NO_THROW(faf.ProduceWith(SubmitSleep));
+    EXPECT_NO_THROW(faf.ProduceWith(SubmitExc));
+
+    EXPECT_ANY_THROW(faf.Finalize());
+
+    EXPECT_EQ(counter, 2);
+}
+
+TEST(Parallel_FireAndForget, exceptionProduceWith)
+{
+    static const size_t numThreads = 3;
+    PacBio::Parallel::FireAndForget faf(numThreads, 1);
+
+    std::atomic_int counter{0};
+    auto SubmitSleep = [&counter]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        ++counter;
+    };
+    auto Submit = [&counter]() { ++counter; };
+    auto SubmitExc = [&counter]() {
+        throw std::runtime_error("faf abort");
+        ++counter;
+    };
+
+    EXPECT_NO_THROW(faf.ProduceWith(SubmitSleep));
+    EXPECT_NO_THROW(faf.ProduceWith(SubmitSleep));
+    EXPECT_NO_THROW(faf.ProduceWith(SubmitExc));
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    EXPECT_ANY_THROW(faf.ProduceWith(Submit););
+    EXPECT_ANY_THROW(faf.Finalize());
+
+    EXPECT_EQ(counter, 2);
 }
