2a4ab8cbee626bccb96738fed454b4e18f441041
Armin Töpfer <atoepfer@pacificbiosciences.com>
2021-12-28 14:30:02 +0100

    [TAG-5334] Add global-local aligner from lima

---
+++ include/pbcopper/align/GlobalLocalAlignment.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace PacBio {
+namespace Align {
+
+struct GlobalLocalParameters
+{
+    int32_t MatchScore{};
+    int32_t MismatchPenalty{};
+    int32_t DeletionPenalty{};
+    int32_t InsertionPenalty{};
+    int32_t BranchPenalty{};
+};
+
+struct GlobalLocalResult
+{
+    int32_t MaxScore = -1;
+    int32_t EndPos = -1;
+};
+
+/// Fills out a supplied dynamic programming matrix.
+void GlobalLocalComputeMatrix(const char* query, int32_t queryLength, const char* target,
+                              int32_t targetLength, bool globalInQuery,
+                              const GlobalLocalParameters& parameters,
+                              std::vector<int32_t>& matrix) noexcept;
+
+/// \brief Align query against target given the parameters.
+///        Global in the query, but missing bases from the query at the start of the target are not penalized.
+///        Local in the target.
+///        Return a max score in the last row and its target position.
+///        Attention: This is the slowest of all available APIs,
+///                   as memory has to allocated for every call.
+GlobalLocalResult GlobalLocalAlign(const std::string& query, const std::string& target,
+                                   const GlobalLocalParameters& parameters) noexcept;
+
+/// \brief Align query against target given the parameters.
+///        Global in the query, but missing bases from the query at the start of the target are not penalized.
+///        Local in the target.
+///        Return a max score in the last row and its target position.
+GlobalLocalResult GlobalLocalAlign(const std::string& query, const std::string& target,
+                                   const GlobalLocalParameters& parameters,
+                                   std::vector<int32_t>& matrix) noexcept;
+
+/// \brief Traverse the last row of a DP matrix (i.e. representing
+///        alignments terminating with the last base of the query
+///        sequence) and return the max score and it's position
+GlobalLocalResult GlobalLocalLastRowMax(const std::vector<int32_t>& matrix, int32_t queryLength,
+                                        int32_t targetLength) noexcept;
+
+}  // namespace Align
+}  // namespace PacBio
---
+++ src/align/GlobalLocalAlignment.cpp
@@ -0,0 +1,131 @@
+#include <pbcopper/align/GlobalLocalAlignment.h>
+
+#include <pbcopper/utility/Ssize.h>
+
+#include <algorithm>
+
+namespace PacBio {
+namespace Align {
+
+GlobalLocalResult GlobalLocalAlign(const std::string& query, const std::string& target,
+                                   const GlobalLocalParameters& parameters) noexcept
+{
+    std::vector<int32_t> matrix;
+    return GlobalLocalAlign(query, target, parameters, matrix);
+}
+
+GlobalLocalResult GlobalLocalAlign(const std::string& query, const std::string& target,
+                                   const GlobalLocalParameters& parameters,
+                                   std::vector<int32_t>& matrix) noexcept
+{
+    const int32_t queryLength = query.size();
+    const int32_t targetLength = target.size();
+    GlobalLocalComputeMatrix(query.c_str(), queryLength, target.c_str(), targetLength, false,
+                             parameters, matrix);
+    return GlobalLocalLastRowMax(matrix, queryLength, targetLength);
+}
+
+void GlobalLocalComputeMatrix(const char* const query, const int32_t queryLength,
+                              const char* const target, const int32_t targetLength,
+                              const bool globalInQuery, const GlobalLocalParameters& parameters,
+                              std::vector<int32_t>& matrix) noexcept
+{
+    // The matrix has m rows for the query and
+    //                n column for the target
+    const int32_t m = queryLength + 1;
+    const int32_t n = targetLength + 1;
+
+    if (Utility::Ssize(matrix) < (m * n)) {
+        matrix.resize(m * n);
+    }
+
+    matrix[0] = 0;
+
+    // Global in query means that we penalize missing bases of the query, if the
+    // target is truncated, only at the target start.
+    // If we don't penalize it, we allow that the target might have been trimmed
+    // in previous processing steps.
+    //
+    // Example
+    // Target:    AGATATGCCAG
+    // Query : GGTAGATAT
+    if (globalInQuery) {
+        for (int32_t i = 1; i < m; ++i) {
+            matrix[i * n] = i * parameters.DeletionPenalty;
+        }
+    } else {
+        for (int32_t i = 1; i < m; ++i) {
+            matrix[i * n] = 0;
+        }
+    }
+
+    for (int32_t j = 1; j < n; ++j) {
+        matrix[j] = 0;
+    }
+
+    // The matrix has i for rows, query bases, and j for columns, target bases.
+    // It's a 1D vector with a row layout. The first n cells are matching the
+    // first query base with 1:m target bases.
+    // Keep in mind that there the first column and row are not matching any
+    // bases but are used for initialization.
+    const int32_t insertionDelta = parameters.BranchPenalty - parameters.InsertionPenalty;
+    for (int32_t i = 1; i < m; ++i) {
+        const char iQuery = query[i];
+        const char iBeforeQuery = query[i - 1];
+        if (i < m - 1) {
+            for (int32_t j = 1; j < n; ++j) {
+                // We prefer more arithmetic operations than accessing memory
+                // in branches.
+                int32_t a{matrix[(i - 1) * n + j - 1]};
+                int32_t b{matrix[i * n + j - 1] + parameters.BranchPenalty};
+                const int32_t c{matrix[(i - 1) * n + j] + parameters.DeletionPenalty};
+                if (target[j - 1] == iBeforeQuery) {
+                    a += parameters.MatchScore;
+                } else {
+                    a += parameters.MismatchPenalty;
+                }
+                if (target[j - 1] != iQuery) {
+                    b -= insertionDelta;
+                }
+                matrix[i * n + j] = std::max(a, std::max(b, c));
+            }
+        } else {
+            for (int32_t j = 1; j < n; ++j) {
+                int32_t a = matrix[(i - 1) * n + j - 1];
+                const int32_t b{matrix[i * n + j - 1] + parameters.InsertionPenalty};
+                const int32_t c{matrix[(i - 1) * n + j] + parameters.DeletionPenalty};
+                if (target[j - 1] == iBeforeQuery) {
+                    a += parameters.MatchScore;
+                } else {
+                    a += parameters.MismatchPenalty;
+                }
+                matrix[i * n + j] = std::max(a, std::max(b, c));
+            }
+        }
+    }
+}
+
+GlobalLocalResult GlobalLocalLastRowMax(const std::vector<int32_t>& matrix,
+                                        const int32_t queryLength,
+                                        const int32_t targetLength) noexcept
+{
+    // Calculate the starting position of the last row
+    const int32_t m = queryLength + 1;
+    const int32_t n = targetLength + 1;
+    const int32_t beginLastRow = (m - 1) * n;
+
+    // Find maximal score in last row and it's position
+    int32_t maxScore = -1;
+    int32_t endPos = 0;
+    for (int32_t j = 0; j < n; ++j) {
+        if (matrix[beginLastRow + j] > maxScore) {
+            maxScore = matrix[beginLastRow + j];
+            endPos = j;
+        }
+    }
+
+    // Return the maximum score and position
+    return {maxScore, endPos};
+}
+}  // namespace Align
+}  // namespace PacBio
