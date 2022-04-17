1ce987054eaef6783c5176971a820d06077ad6a5
David Seifert <dseifert@pacificbiosciences.com>
2022-04-17 21:41:21 +0200

    [TAG-5488] Add `PacBio::Cuda` for GPU allocators/deleters

---
+++ include/pbcopper/cuda/Cuda.h
@@ -0,0 +1,90 @@
+#ifndef PBCOPPER_CUDA_CUDA_H
+#define PBCOPPER_CUDA_CUDA_H
+
+#include <cuda_runtime_api.h>
+
+#include <cassert>
+#include <cstddef>
+#include <stdexcept>
+
+namespace PacBio {
+namespace Cuda {
+
+//
+// Convenience allocators/deleters intended for use with GPU code
+//
+
+// Host memory
+struct PinnedWriteCombinedMemoryAllocator
+{
+    std::byte* operator()(const ptrdiff_t size) const
+    {
+        assert(size >= 0);
+
+        std::byte* ptr = nullptr;
+        const auto status =
+            cudaHostAlloc(reinterpret_cast<void**>(&ptr), size, cudaHostAllocWriteCombined);
+        if (status != cudaSuccess) {
+            throw std::runtime_error(
+                "[pbcopper] ERROR: error allocating write-combined pinned memory");
+        }
+
+        return ptr;
+    }
+};
+
+struct PinnedMemoryAllocator
+{
+    std::byte* operator()(const ptrdiff_t size) const
+    {
+        assert(size >= 0);
+
+        std::byte* ptr = nullptr;
+        const auto status = cudaMallocHost(reinterpret_cast<void**>(&ptr), size);
+        if (status != cudaSuccess) {
+            throw std::runtime_error("[pbcopper] ERROR: error allocating pinned memory");
+        }
+
+        return ptr;
+    }
+};
+
+struct PinnedMemoryDeleter
+{
+    void operator()(void* ptr) const noexcept
+    {
+        // counter-part to cudaMallocHost on the CPU
+        cudaFreeHost(ptr);
+    }
+};
+
+// Device memory
+struct DeviceMemoryAllocator
+{
+    std::byte* operator()(const ptrdiff_t size) const
+    {
+        assert(size >= 0);
+
+        std::byte* ptr = nullptr;
+        const auto status = cudaMalloc(reinterpret_cast<void**>(&ptr), size);
+        if (status != cudaSuccess) {
+            throw std::runtime_error("[pbcopper] ERROR: error allocating device memory");
+        }
+
+        return ptr;
+    }
+};
+
+struct GPUMemoryDeleter
+{
+    void operator()(void* ptr) const noexcept
+    {
+        // counter-part to cudaMalloc on the GPU
+        cudaFree(ptr);
+    }
+};
+
+}  // namespace Cuda
+}  // namespace PacBio
+
+#endif  // PBCOPPER_CUDA_CUDA_H
