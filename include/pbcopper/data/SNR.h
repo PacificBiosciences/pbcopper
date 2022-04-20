6fb2cf748ae7ead2470d3d09d88272037ebfc6f5
David Seifert <dseifert@pacificbiosciences.com>
2022-04-20 14:08:41 +0200

    [TAG-5488] Allow `Data::ClampSNR` in CUDA

--- include/pbcopper/data/SNR.h
+++ include/pbcopper/data/SNR.h
@@ -21,7 +21,8 @@ namespace Data {
 class alignas(16) SNR
 {
 public:
-    constexpr SNR(const float a, const float c, const float g, const float t) noexcept
+    PB_CUDA_HOST PB_CUDA_DEVICE constexpr SNR(const float a, const float c, const float g,
+                                              const float t) noexcept
         : data_{a, c, g, t}
     {}
 
@@ -30,20 +31,22 @@ public:
         assert(snrs.size() == 4);
     }
 
-    constexpr SNR(const float (&snrs)[4]) noexcept : SNR{snrs[0], snrs[1], snrs[2], snrs[3]} {}
+    PB_CUDA_HOST PB_CUDA_DEVICE constexpr SNR(const float (&snrs)[4]) noexcept
+        : SNR{snrs[0], snrs[1], snrs[2], snrs[3]}
+    {}
 
 public:
     operator std::vector<float>() const;
 
 public:
-    constexpr const float& operator[](const int32_t i) const noexcept
+    PB_CUDA_HOST PB_CUDA_DEVICE constexpr const float& operator[](const int32_t i) const noexcept
     {
         assert(i >= 0);
         assert(i < 4);
         return data_[i];
     }
 
-    constexpr float& operator[](const int32_t i) noexcept
+    PB_CUDA_HOST PB_CUDA_DEVICE constexpr float& operator[](const int32_t i) noexcept
     {
         // casting away const when underlying object is non-const, is well-defined
         // Effective C++ (Third Edition) - Item 3
@@ -74,7 +77,22 @@ private:
 
 constexpr bool operator!=(const SNR& lhs, const SNR& rhs) noexcept { return !(lhs == rhs); }
 
-SNR ClampSNR(const SNR& val, const SNR& min, const SNR& max) noexcept;
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr SNR ClampSNR(SNR v, const SNR& lo, const SNR& hi) noexcept
+{
+#ifndef __CUDA_ARCH__  // host
+    using std::max;
+    using std::min;
+#endif
+
+    for (int32_t i = 0; i < 4; ++i) {
+        const float lowerBound = lo[i];
+        const float upperBound = hi[i];
+        assert(lowerBound < upperBound);
+
+        v[i] = max(lowerBound, min(upperBound, v[i]));
+    }
+    return v;
+}
 
 std::ostream& operator<<(std::ostream& os, const SNR& snr);
 
--- src/data/SNR.cpp
+++ src/data/SNR.cpp
@@ -1,7 +1,5 @@
 #include <pbcopper/data/SNR.h>
 
-#include <boost/algorithm/clamp.hpp>
-
 #include <ostream>
 #include <type_traits>
 
@@ -11,14 +9,6 @@ namespace Data {
 static_assert(std::is_trivially_copyable_v<SNR>, "SNR is not a trivially copyable type");
 
 SNR::operator std::vector<float>() const { return {data_[0], data_[1], data_[2], data_[3]}; }
-
-SNR ClampSNR(const SNR& val, const SNR& lo, const SNR& hi) noexcept
-{
-    return SNR{boost::algorithm::clamp(val[0], lo[0], hi[0]),
-               boost::algorithm::clamp(val[1], lo[1], hi[1]),
-               boost::algorithm::clamp(val[2], lo[2], hi[2]),
-               boost::algorithm::clamp(val[3], lo[3], hi[3])};
-}
 
 std::ostream& operator<<(std::ostream& os, const SNR& snr)
 {
