911a5f3f28649c587231983ebd07f60e3543f7c3
David Seifert <dseifert@pacificbiosciences.com>
2022-05-10 16:05:35 +0200

    [TAG-5672] CUDA ASCII ⇆ 2-bit routines

---
+++ include/pbcopper/cuda/AsciiConversion.h
@@ -0,0 +1,62 @@
+#ifndef PBCOPPER_CUDA_ASCIICONVERSION_H
+#define PBCOPPER_CUDA_ASCIICONVERSION_H
+
+#include <pbcopper/PbcopperConfig.h>
+
+#include <pbcopper/container/BitConversion.h>
+#include <pbcopper/container/BitmaskContainer.h>
+
+#include <cstdint>
+
+namespace PacBio {
+namespace Cuda {
+
+template <int32_t TotalBits, int32_t ElementBits, int32_t N>
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr Container::BitmaskContainer<TotalBits, ElementBits>
+AsciiToBitmaskContainer(const uint32_t (&in)[N], const int32_t len)
+{
+    Container::BitmaskContainer<TotalBits, ElementBits> result;
+    static_assert(result.Capacity() >= (N * 4),
+                  "Return type does not have sufficient capacity for provided ASCII bases");
+
+    Container::BitmaskContainer<32, 8> buf;
+    for (int32_t i = 0; i < (N * 4); ++i) {
+        if ((i % 4) == 0) {
+            buf = Container::BitmaskContainer<32, 8>{in[i / 4]};
+        }
+
+        const char base = buf[i % 4];
+        const int32_t encodedBase{(i < len) ? Container::ConvertAsciiTo2bit(base) : 0};
+        result.Set(i, encodedBase);
+    }
+
+    return result;
+}
+
+template <int32_t TotalBits, int32_t ElementBits, int32_t N>
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr void BitmaskContainerToAscii(
+    const Container::BitmaskContainer<TotalBits, ElementBits> in, const int32_t len,
+    uint32_t (&out)[N])
+{
+    static_assert((N * 4) >= in.Capacity(),
+                  "Out array does not have sufficient capacity for provided compressed string");
+
+    Container::BitmaskContainer<32, 8> buf;
+    for (int32_t i = 0; i < (N * 4); ++i) {
+        if ((i % 4) == 0) {
+            buf.Clear();
+        }
+
+        const char base = Container::Convert2bitToAscii(in[i]);
+        buf.Set(i % 4, (i < len) ? base : 0);
+
+        if ((i + 1) % 4 == 0) {
+            out[i / 4] = buf.RawData();
+        }
+    }
+}
+
+}  // namespace Cuda
+}  // namespace PacBio
+
+#endif  // PBCOPPER_CUDA_ASCIICONVERSION_H
---
+++ tests/src/cuda/test_AsciiConversion.cpp
@@ -0,0 +1,82 @@
+#include <pbcopper/cuda/AsciiConversion.h>
+
+#include <gtest/gtest.h>
+
+#include <pbcopper/container/DNAString.h>
+
+#include <array>
+#include <cstdint>
+#include <cstring>
+#include <random>
+#include <string>
+
+using namespace PacBio;
+
+TEST(Cuda_AsciiConversion, SingleBase)
+{
+    {
+        uint32_t arr[8] = {'A'};
+        const auto result = Cuda::AsciiToBitmaskContainer<64, 2>(arr, 1);
+        EXPECT_EQ(result.RawData(), 0);
+    }
+
+    {
+        uint32_t arr[8] = {'C'};
+        const auto result = Cuda::AsciiToBitmaskContainer<64, 2>(arr, 1);
+        EXPECT_EQ(result.RawData(), 1);
+    }
+
+    {
+        uint32_t arr[8] = {'G'};
+        const auto result = Cuda::AsciiToBitmaskContainer<64, 2>(arr, 1);
+        EXPECT_EQ(result.RawData(), 2);
+    }
+
+    {
+        uint32_t arr[8] = {'T'};
+        const auto result = Cuda::AsciiToBitmaskContainer<64, 2>(arr, 1);
+        EXPECT_EQ(result.RawData(), 3);
+    }
+}
+
+TEST(Cuda_AsciiConversion, Comprehensive)
+{
+    std::random_device rd;
+    std::mt19937 gen{rd()};
+
+    const int32_t rounds = 100'000;
+
+    constexpr std::array<char, 4> DNA_BASES{'A', 'C', 'G', 'T'};
+    std::uniform_int_distribution<int32_t> baseDistribution{0, 3};
+    std::uniform_int_distribution<int32_t> lengthDistribution{0, 31};
+
+    for (int32_t r = 0; r < rounds; ++r) {
+        // generate a random ASCII string
+        const int32_t length = lengthDistribution(gen);
+        std::string ascii1;
+        for (int32_t i = 0; i < length; ++i) {
+            ascii1.push_back(DNA_BASES[baseDistribution(gen)]);
+        }
+
+        uint32_t arr1[8] = {};
+        std::memcpy(arr1, ascii1.c_str(), length);
+
+        // test CUDA ASCII → 2-bit conversion
+        const auto comp1 = Cuda::AsciiToBitmaskContainer<64, 2>(arr1, length);
+        const Container::DNA2bitStringVariable64 dna1{ascii1};
+        EXPECT_EQ(dna1.RawData(), comp1.RawData());
+        EXPECT_EQ(dna1.Length(), length);
+
+        // test CUDA 2-bit conversion → ASCII
+        uint32_t arr2[8] = {};
+        Cuda::BitmaskContainerToAscii(comp1, length, arr2);
+        EXPECT_EQ(std::strcmp(ascii1.c_str(), reinterpret_cast<const char*>(arr2)), 0);
+        static_assert(sizeof(arr1) == 32);
+        EXPECT_EQ(std::memcmp(arr1, arr2, sizeof(arr1)), 0);
+
+        // test CUDA ASCII → 2-bit conversion (roundtrip)
+        const auto comp2 = Cuda::AsciiToBitmaskContainer<64, 2>(arr2, length);
+        EXPECT_EQ(dna1.RawData(), comp2.RawData());
+        EXPECT_EQ(comp1, comp2);
+    }
+}
