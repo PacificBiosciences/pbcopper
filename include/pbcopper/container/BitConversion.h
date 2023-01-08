1d140b4fb617715a4b38cd803a2211c23e68dca3
David Seifert <dseifert@pacificbiosciences.com>
2023-01-08 10:32:34 +0100

    [TAK-1794] Allow complement/checking in `Container::ConvertAsciiTo2bit`
    * Also allow lowercase characters

--- include/pbcopper/container/BitConversion.h
+++ include/pbcopper/container/BitConversion.h
@@ -29,22 +29,68 @@ namespace Container {
 // Reference:
 //   https://www.ncbi.nlm.nih.gov/IEB/ToolBox/SDKDOCS/BIOSEQ.HTML
 //
-// Clang 13 (amd64; -march=znver2 -O3 -DNDEBUG): 14 bytes L1i
-//   0:   b8 08 01 30 00          mov    eax,0x300108
-//   5:   c4 e2 43 f7 c0          shrx   eax,eax,edi
-//   a:   83 e0 03                and    eax,0x3
-//   d:   c3                      ret
-PB_CUDA_HOST PB_CUDA_DEVICE constexpr int32_t ConvertAsciiTo2bit(const char base) noexcept
+// * checked == false:
+//   The input is not checked for validity. Passing a non-DNA
+//   character into the function does **NOT** cause UB, but yields
+//   an unpredictable value from the set {0, 1, 2, 3}.
+//
+//   Clang 13 (amd64; -march=znver2 -O3 -DNDEBUG): 14 bytes L1i
+//     0:   b8 08 01 30 00          mov    eax,0x300108
+//     5:   c4 e2 43 f7 c0          shrx   eax,eax,edi
+//     a:   83 e0 03                and    eax,0x3
+//     d:   c3                      ret
+//
+// * checked == true:
+//   The input is checked for validity. Passing a non-DNA character
+//   yields a return value >=4.
+//
+//   Clang 13 (amd64; -march=znver2 -O3 -DNDEBUG): 52 bytes L1i
+//     0:   b8 75 ff ef ff          mov    eax,0xffefff75
+//     5:   ba 44 00 44 44          mov    edx,0x44440044
+//     a:   c4 e2 43 f7 c8          shrx   ecx,eax,edi
+//     f:   b8 08 01 30 00          mov    eax,0x300108
+//    14:   c4 e2 43 f7 c0          shrx   eax,eax,edi
+//    19:   40 c0 ef 03             shr    dil,0x3
+//    1d:   c1 e1 02                shl    ecx,0x2
+//    20:   40 80 e7 1c             and    dil,0x1c
+//    24:   83 e0 03                and    eax,0x3
+//    27:   c4 e2 43 f7 d2          shrx   edx,edx,edi
+//    2c:   09 ca                   or     edx,ecx
+//    2e:   83 e2 04                and    edx,0x4
+//    31:   09 d0                   or     eax,edx
+//    33:   c3                      ret
+template <bool complement = false, bool checked = false>
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr int32_t ConvertAsciiTo2bit(const unsigned char base) noexcept
 {
-    assert((base == 'A') || (base == 'C') || (base == 'G') || (base == 'T'));
-
     const int32_t truncatedBase = base & 0b1'1111;
 
-    // ASCII range [64, 85)
-    constexpr uint32_t LOOKUP_TABLE{//  ┝T┥                       ┝G┥     ┝C┥ ┝A┥
-                                    0b0'1'1'0'0'0'0'0'0'0'0'0'0'0'1'0'0'0'0'1'0'0'0};
+    uint32_t invalidBits = 0;
+    if constexpr (checked) {
+        // check the least-significant 5 bits
+        constexpr auto LSB_INVALID_LOOKUP_TABLE = Container::BitmaskContainer<32, 1>::MakeFromArray(
+            1, 0 /*A*/, 1, 0 /*C*/, 1, 1, 1, 0 /*G*/, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 /*T*/,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+        const uint32_t lsbCheckResult = 4 * LSB_INVALID_LOOKUP_TABLE[truncatedBase];
 
-    return (LOOKUP_TABLE >> truncatedBase) & 0b11;
+        // check the most-significant 3 bits
+        constexpr auto MSB_INVALID_LOOKUP_TABLE =
+            Container::BitmaskContainer<32, 4>::MakeFromArray(4, 4, 0, 0, 4, 4, 4, 4);
+        const uint32_t msbCheckResult = MSB_INVALID_LOOKUP_TABLE[base >> 5];
+
+        invalidBits = lsbCheckResult | msbCheckResult;
+    } else {
+        assert((base == 'A') || (base == 'C') || (base == 'G') || (base == 'T') || (base == 'a') ||
+               (base == 'c') || (base == 'g') || (base == 't'));
+    }
+
+    constexpr uint32_t LOOKUP_TABLE{complement
+                                        //  _^]\[ZYXWVUTSRQPONMLKJIHGFEDCBA@ [64, 96)
+                                        //   ~}|{zyxwvutsrqponmlkjihgfedcba` [96, 128)
+                                        //            ┌┤           ┌┤  ┌┤┌┤
+                                        ? 0b00000000000000000000000010010110
+                                        : 0b00000000001100000000000100001000};
+
+    return invalidBits | ((LOOKUP_TABLE >> truncatedBase) & 0b11);
 }
 
 // Convert 2-bit encoding (NCBI2na) to ASCII
@@ -56,9 +102,6 @@ PB_CUDA_HOST PB_CUDA_DEVICE constexpr int32_t ConvertAsciiTo2bit(const char base
 //   d:   c3                      ret
 PB_CUDA_HOST PB_CUDA_DEVICE constexpr char Convert2bitToAscii(const int32_t val) noexcept
 {
-    assert(val >= 0);
-    assert(val < 4);
-
     constexpr auto LOOKUP_TABLE =
         Container::BitmaskContainer<32, 8>::MakeFromArray('A', 'C', 'G', 'T');
     return LOOKUP_TABLE[val];
--- tests/src/container/test_BitConversion.cpp
+++ tests/src/container/test_BitConversion.cpp
@@ -6,32 +6,140 @@ using namespace PacBio::Container;
 
 TEST(Container_BitConversion, ConvertAsciiTo2bit)
 {
-    EXPECT_EQ(ConvertAsciiTo2bit('A'), 0);
-    EXPECT_EQ(ConvertAsciiTo2bit('C'), 1);
-    EXPECT_EQ(ConvertAsciiTo2bit('G'), 2);
-    EXPECT_EQ(ConvertAsciiTo2bit('T'), 3);
+    const auto f = ConvertAsciiTo2bit<false>;
+
+    EXPECT_EQ(f('A'), 0);
+    EXPECT_EQ(f('a'), 0);
+    EXPECT_EQ(f('C'), 1);
+    EXPECT_EQ(f('c'), 1);
+    EXPECT_EQ(f('G'), 2);
+    EXPECT_EQ(f('g'), 2);
+    EXPECT_EQ(f('T'), 3);
+    EXPECT_EQ(f('t'), 3);
+}
+
+TEST(Container_BitConversion, ConvertAsciiTo2bitComplement)
+{
+    const auto f = ConvertAsciiTo2bit<true>;
+
+    EXPECT_EQ(f('A'), 3);
+    EXPECT_EQ(f('a'), 3);
+    EXPECT_EQ(f('C'), 2);
+    EXPECT_EQ(f('c'), 2);
+    EXPECT_EQ(f('G'), 1);
+    EXPECT_EQ(f('g'), 1);
+    EXPECT_EQ(f('T'), 0);
+    EXPECT_EQ(f('t'), 0);
+}
+
+TEST(Container_BitConversion, ConvertAsciiTo2bitChecked)
+{
+    const auto f = ConvertAsciiTo2bit<false, true>;
+
+    for (int32_t i = 0; i < 256; ++i) {
+        switch (i) {
+            case 'A':
+            case 'a':
+                EXPECT_EQ(f(i), 0);
+                break;
+            case 'C':
+            case 'c':
+                EXPECT_EQ(f(i), 1);
+                break;
+            case 'G':
+            case 'g':
+                EXPECT_EQ(f(i), 2);
+                break;
+            case 'T':
+            case 't':
+                EXPECT_EQ(f(i), 3);
+                break;
+            default:
+                EXPECT_GE(f(i), 4);
+                break;
+        }
+    }
+}
+
+TEST(Container_BitConversion, ConvertAsciiTo2bitComplementChecked)
+{
+    const auto f = ConvertAsciiTo2bit<true, true>;
+
+    for (int32_t i = 0; i < 256; ++i) {
+        switch (i) {
+            case 'A':
+            case 'a':
+                EXPECT_EQ(f(i), 3);
+                break;
+            case 'C':
+            case 'c':
+                EXPECT_EQ(f(i), 2);
+                break;
+            case 'G':
+            case 'g':
+                EXPECT_EQ(f(i), 1);
+                break;
+            case 'T':
+            case 't':
+                EXPECT_EQ(f(i), 0);
+                break;
+            default:
+                EXPECT_GE(f(i), 4);
+                break;
+        }
+    }
 }
 
 TEST(Container_BitConversion, Convert2bitToAscii)
 {
-    EXPECT_EQ(Convert2bitToAscii(0), 'A');
-    EXPECT_EQ(Convert2bitToAscii(1), 'C');
-    EXPECT_EQ(Convert2bitToAscii(2), 'G');
-    EXPECT_EQ(Convert2bitToAscii(3), 'T');
+    const auto f = Convert2bitToAscii;
+
+    EXPECT_EQ(f(0), 'A');
+    EXPECT_EQ(f(1), 'C');
+    EXPECT_EQ(f(2), 'G');
+    EXPECT_EQ(f(3), 'T');
 }
 
 TEST(Container_BitConversion, RoundRobin2bit)
 {
-    EXPECT_EQ(ConvertAsciiTo2bit(Convert2bitToAscii(0)), 0);
-    EXPECT_EQ(ConvertAsciiTo2bit(Convert2bitToAscii(1)), 1);
-    EXPECT_EQ(ConvertAsciiTo2bit(Convert2bitToAscii(2)), 2);
-    EXPECT_EQ(ConvertAsciiTo2bit(Convert2bitToAscii(3)), 3);
+    const auto innerF = Convert2bitToAscii;
+    const auto outerF = ConvertAsciiTo2bit<false>;
+
+    EXPECT_EQ(outerF(innerF(0)), 0);
+    EXPECT_EQ(outerF(innerF(1)), 1);
+    EXPECT_EQ(outerF(innerF(2)), 2);
+    EXPECT_EQ(outerF(innerF(3)), 3);
+}
+
+TEST(Container_BitConversion, RoundRobin2bitComplement)
+{
+    const auto innerF = Convert2bitToAscii;
+    const auto outerF = ConvertAsciiTo2bit<true>;
+
+    EXPECT_EQ(outerF(innerF(0)), 3);
+    EXPECT_EQ(outerF(innerF(1)), 2);
+    EXPECT_EQ(outerF(innerF(2)), 1);
+    EXPECT_EQ(outerF(innerF(3)), 0);
 }
 
 TEST(Container_BitConversion, RoundRobinAscii)
 {
-    EXPECT_EQ(Convert2bitToAscii(ConvertAsciiTo2bit('A')), 'A');
-    EXPECT_EQ(Convert2bitToAscii(ConvertAsciiTo2bit('C')), 'C');
-    EXPECT_EQ(Convert2bitToAscii(ConvertAsciiTo2bit('G')), 'G');
-    EXPECT_EQ(Convert2bitToAscii(ConvertAsciiTo2bit('T')), 'T');
+    const auto innerF = ConvertAsciiTo2bit<false>;
+    const auto outerF = Convert2bitToAscii;
+
+    EXPECT_EQ(outerF(innerF('A')), 'A');
+    EXPECT_EQ(outerF(innerF('C')), 'C');
+    EXPECT_EQ(outerF(innerF('G')), 'G');
+    EXPECT_EQ(outerF(innerF('T')), 'T');
+}
+
+TEST(Container_BitConversion, RoundRobinAsciiComplement)
+{
+    const auto innerF = ConvertAsciiTo2bit<true>;
+    const auto outerF = Convert2bitToAscii;
+
+    EXPECT_EQ(outerF(innerF('A')), 'T');
+    EXPECT_EQ(outerF(innerF('C')), 'G');
+    EXPECT_EQ(outerF(innerF('G')), 'C');
+    EXPECT_EQ(outerF(innerF('T')), 'A');
 }
