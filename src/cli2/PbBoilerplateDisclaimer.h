c1f21560306db9094c6c310371458a7d7c1ff66f
David Seifert <dseifert@pacificbiosciences.com>
2023-01-18 23:34:49 +0100

    [TAK-1739] Update year in disclaimer

--- src/cli2/PbBoilerplateDisclaimer.h
+++ src/cli2/PbBoilerplateDisclaimer.h
@@ -2,7 +2,7 @@
 #define PBCOPPER_PBBOILERPLATEDISCLAIMER_H
 
 #define PB_BOILERPLATE_DISCLAIMER                                                              \
-    "Copyright (C) 2004-2022     Pacific Biosciences of California, Inc.\nThis program comes " \
+    "Copyright (C) 2004-2023     Pacific Biosciences of California, Inc.\nThis program comes " \
     "with ABSOLUTELY NO WARRANTY; it is intended for\nResearch Use Only and not for use in "   \
     "diagnostic procedures.\n"
 
