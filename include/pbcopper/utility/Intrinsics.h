307055e9036a377fc06713862ece03a14aa0ae2d
David Seifert <dseifert@pacificbiosciences.com>
2023-07-07 16:52:41 +0200

    [BFX-241] utility/Intrinsics.h: enable for CUDA

--- include/pbcopper/utility/Intrinsics.h
+++ include/pbcopper/utility/Intrinsics.h
@@ -3,8 +3,12 @@
 
 #include <pbcopper/PbcopperConfig.h>
 
-#include <cstdint>
 #include <type_traits>
+#include <version>
+
+#include <cassert>
+#include <climits>
+#include <cstdint>
 
 /*
  * Because C++-20 bit operations are not standard across compilers, this header
@@ -29,42 +33,81 @@ namespace Utility {
 
 // PopCount
 
+// * std::int8_t/uint8_t
+// * std::int16_t/uint16_t
+// * std::int32_t/uint32_t
 template <typename T>
-int PopCount(T x) noexcept
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr
+    typename std::enable_if_t<std::is_integral_v<T> && (sizeof(T) <= 4), int>
+    PopCount(const T x) noexcept
 {
-    static_assert(
-        std::is_integral_v<T> || std::is_same_v<T, __uint128_t> || std::is_same_v<T, __int128_t>,
-        "Must be integral, U128 or I128");
-    if constexpr (sizeof(T) == 16) {
-        return PopCount(static_cast<std::uint64_t>(x)) +
-               PopCount(static_cast<std::uint64_t>(x >> 64));
-    } else {
-#ifdef PB_BITOPS_ENABLED
-        if constexpr (std::is_integral_v<T>) {
-            return std::popcount(static_cast<std::make_unsigned_t<T>>(x));
-        } else {
-            return PopCount(std::uint64_t(x)) + PopCount(std::uint64_t(x >> 64));
-        }
+    // For std::int8_t and std::int16_t, we first have to convert to the unsigned type
+    // of the same rank, and then cast to std::uint32_t, since we would
+    // sign-extend x (increasing the number of set bits) to 32 bits otherwise.
+    const unsigned int paddedX = std::make_unsigned_t<T>(x);
+
+    return
+#ifdef __CUDA_ARCH__
+        // device
+        __popc(paddedX)
 #else
-        if constexpr (std::is_same_v<std::make_signed_t<T>, long signed int>) {
-            return __builtin_popcountl(x);
-        } else if constexpr (std::is_same_v<std::make_signed_t<T>, long long signed int>) {
-            return __builtin_popcountll(x);
-        } else {
-            return __builtin_popcount(static_cast<int>(x));
-        }
-#endif
-    }
+#ifdef PB_BITOPS_ENABLED
+        // C++20
+        std::popcount(paddedX)
+#else
+        // C++17 and below
+        // TODO: remove once we're on C++20 in CUDA too
+        __builtin_popcount(paddedX)
+#endif  // PB_BITOPS_ENABLED
+#endif  // __CUDA_ARCH__
+            ;
+}
+
+// * std::int64_t/uint64_t
+template <typename T>
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr
+    typename std::enable_if_t<std::is_integral_v<T> && (sizeof(T) == 8), int>
+    PopCount(const T x) noexcept
+{
+    const unsigned long long int val = x;
+
+    return
+#ifdef __CUDA_ARCH__
+        // device
+        __popcll(val)
+#else
+#ifdef PB_BITOPS_ENABLED
+        // C++20
+        std::popcount(val)
+#else
+        // C++17 and below
+        // TODO: remove once we're on C++20 in CUDA too
+        __builtin_popcountll(val)
+#endif  // PB_BITOPS_ENABLED
+#endif  // __CUDA_ARCH__
+            ;
+}
+
+// * __int128_t/__uint128_t
+template <typename T>
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr
+    typename std::enable_if_t<std::is_same_v<T, __uint128_t> || std::is_same_v<T, __int128_t>, int>
+    PopCount(const T x) noexcept
+{
+    const __uint128_t val = x;
+
+    return PopCount(std::uint64_t(val)) + PopCount(std::uint64_t(val >> 64));
 }
 
 // Count Trailing Zeros
+// no __CUDA_ARCH__ code paths, since CUDA has no __builtin_ctz equivalent
 
 template <typename T>
 int CountTrailingZeros(T x) noexcept
 {
     if constexpr (sizeof(T) == 16) {
         const int trailingLow = CountTrailingZeros(std::uint64_t(x));
-        if (trailingLow != static_cast<int>(sizeof(T) * 8)) {
+        if (trailingLow != static_cast<int>(sizeof(T) * CHAR_BIT)) {
             return trailingLow;
         }
         return CountTrailingZeros(std::uint64_t(x >> 64)) + 64;
@@ -83,34 +126,89 @@ int CountTrailingZeros(T x) noexcept
     }
 }
 
+// Count Leading Zeros
+
+// * std::int8_t/uint8_t
+// * std::int16_t/uint16_t
+// * std::int32_t/uint32_t
 template <typename T>
-int CountLeadingZeros(T x) noexcept
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr
+    typename std::enable_if_t<std::is_integral_v<T> && (sizeof(T) <= 4), int>
+    CountLeadingZeros(const T x) noexcept
 {
-    if constexpr (sizeof(T) == 16) {
-        const int leadingHi = CountLeadingZeros(std::uint64_t(x >> 64));
-        if (leadingHi != static_cast<int>(sizeof(T) * 8)) {
-            return leadingHi;
-        }
-        return CountLeadingZeros(std::uint64_t(x)) + 64;
-    } else {
-#ifdef PB_BITOPS_ENABLED
-        return std::countl_zero(x);
+    // For std::int8_t and std::int16_t, we first have to convert to the unsigned type
+    // of the same rank, and then cast to std::uint32_t, since we would
+    // sign-extend x (increasing the number of set bits) to 32 bits otherwise.
+    const auto unsignedX = std::make_unsigned_t<T>(x);
+    [[maybe_unused]] const unsigned int paddedX = unsignedX;
+
+    [[maybe_unused]] constexpr int NUM_PADDED_BITS = (4 - sizeof(T)) * CHAR_BIT;
+
+    return
+#ifdef __CUDA_ARCH__
+        // device
+        __clz(paddedX) - NUM_PADDED_BITS
 #else
-        if constexpr (std::is_same_v<std::make_signed_t<T>, long signed int>) {
-            return __builtin_clzl(x);
-        } else if constexpr (std::is_same_v<std::make_signed_t<T>, long long signed int>) {
-            return __builtin_clzll(x);
-        } else {
-            return __builtin_clz(static_cast<int>(x));
-        }
-#endif
+#ifdef PB_BITOPS_ENABLED
+        // C++20
+        std::countl_zero(unsignedX)
+#else
+        // C++17 and below
+        // TODO: remove once we're on C++20 in CUDA too
+        // UB if the input is 0
+        //   https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html#index-_005f_005fbuiltin_005fclz
+        (paddedX ? __builtin_clz(paddedX) : 32) - NUM_PADDED_BITS
+#endif  // PB_BITOPS_ENABLED
+#endif  // __CUDA_ARCH__
+        ;
+}
+
+// * std::int64_t/uint64_t
+template <typename T>
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr
+    typename std::enable_if_t<std::is_integral_v<T> && (sizeof(T) == 8), int>
+    CountLeadingZeros(const T x) noexcept
+{
+    const unsigned long long int val = x;
+
+    return
+#ifdef __CUDA_ARCH__
+        // device
+        __clzll(val)
+#else
+#ifdef PB_BITOPS_ENABLED
+        // C++20
+        std::countl_zero(val)
+#else
+        // C++17 and below
+        // TODO: remove once we're on C++20 in CUDA too
+        // UB if the input is 0
+        //   https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html#index-_005f_005fbuiltin_005fclzll
+        val ? __builtin_clzll(val) : 64
+#endif  // PB_BITOPS_ENABLED
+#endif  // __CUDA_ARCH__
+            ;
+}
+
+// * __int128_t/__uint128_t
+template <typename T>
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr
+    typename std::enable_if_t<std::is_same_v<T, __uint128_t> || std::is_same_v<T, __int128_t>, int>
+    CountLeadingZeros(const T x) noexcept
+{
+    const __uint128_t val = x;
+
+    const int leadingHi = CountLeadingZeros(std::uint64_t(val >> 64));
+    if (leadingHi != static_cast<int>(sizeof(T) * CHAR_BIT)) {
+        return leadingHi;
     }
+    return CountLeadingZeros(std::uint64_t(val)) + 64;
 }
 
 // We could use std::has_single_bit, but this is simpler
 // and has_single_bit implementations are usually based on popcount
 template <typename T>
-bool HasSingleBit(T x) noexcept
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr bool HasSingleBit(const T x) noexcept
 {
     return PopCount(x) == 1;
 }
@@ -144,11 +242,13 @@ T BitCeiling(T x) noexcept
     }
     return ++x;
 }
-// Only correct if x > 0, but fast
+
 template <typename T>
-int IntegralLog2(T x)
+PB_CUDA_HOST PB_CUDA_DEVICE constexpr int IntegralLog2(const T x)
 {
-    return sizeof(T) * 8 - Utility::CountLeadingZeros(x) - 1;
+    // UB if x <= 0
+    assert(x > 0);
+    return sizeof(T) * CHAR_BIT - Utility::CountLeadingZeros(x) - 1;
 }
 
 }  // namespace Utility
